ce70ab2c3e2814179985b09eeaf1bd05b49ebd1f
Lorena Moesch <ilmoesch@gmail.com>
2021-06-18 02:47:58 -0300

    save branch class-5a

--- services/database/apod.mongo.service.js
+++ services/database/apod.mongo.service.js
@@ -3,14 +3,14 @@ const apod = require('../../config/Schema/apod.schema');
 async function saveApod(){
     const apodToday = new apod({title: 'my apod'});
     try{
-        await apodToday.save((err, apodToday) => {
-            console.log('new element added to the DB', apodToday);
+        await apodToday.save((err, element) => {
+            console.log('new element added to the DB', element);
         });
     } catch(err){
         throw err;
     }
 
-    return {status: 'ok'};
+    return {status: 'ok prueba task'};
 };
 
 module.exports = {saveApod};
